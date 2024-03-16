9ed830be0ccfcfc9d572ca28ad05412fd5d1ae89
Igwe Ugochukwu Michael Sylvester <96844092+elinteerie@users.noreply.github.com>
2024-03-16 19:05:13 +0100

    add

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,9 +1,42 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Commodity
-
+from datetime import timedelta
+import decimal
 
 class CommoditySerializer(serializers.ModelSerializer):
+    percentage_change = serializers.SerializerMethodField()
     class Meta:
         model = Commodity
+        fields = ['name', 'current_price', 'time', 'percentage_change']
 
+    def get_percentage_change(self, instance):
+        current_price = instance.current_price
+
+        previous_price = self.get_previous_price
+
+        if previous_price:
+            percentage_change = ((current_price - previous_price) / previous_price) * 100
+            return percentage_change
+        else: 
+            return None
+        
+    def get_previous_price(self, instance):
+
+        if instance.time:
+           one_hour_ago = decimal(200)
+           one_day_ago = decimal(500)
+           #one_hour_ago = instance.time - timedelta(hours=1)
+           #one_day_ago = instance.time - timedelta(days=1)
+
+           if instance >one_hour_ago:
+               print(one_hour_ago)
+               return one_hour_ago
+           elif instance.time >one_day_ago:
+               return one_day_ago
+        return None
+
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        return {representation['name']: representation}
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from django.utils import timezone
+from datetime import timedelta
+from .models import Commodity
+
+from .models import Commodity
+from .serializers import CommoditySerializer
+
+
 
 # Create your views here.
+class CommodityListCreateView(generics.ListCreateAPIView):
+    queryset = Commodity.objects.all()
+    serializer_class = CommoditySerializer
+
+
+# In your Django app's views.py file
+
+
+class CommodityDetailsAPIView(generics.ListAPIView):
+    queryset = Commodity.objects.all()
+    serializer_class = CommoditySerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        # Group commodities by commodity_name
+        grouped_commodities = {}
+        for commodity in queryset:
+            commodity_name = commodity.name
+            if commodity_name not in grouped_commodities:
+                grouped_commodities[commodity_name] = []
+            grouped_commodities[commodity_name].append(commodity)
+
+        # Serialize each group separately
+        serialized_data = {}
+        for commodity_name, commodities in grouped_commodities.items():
+            serialized_data[commodity_name] = self.serializer_class(commodities, many=True).data
+
+        return Response(serialized_data)
+
