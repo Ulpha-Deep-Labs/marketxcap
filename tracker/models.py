b7d5d717a90edcd4250ada55bea38631e62edef1
Igwe Ugochukwu Michael Sylvester <96844092+elinteerie@users.noreply.github.com>
2024-03-16 19:48:23 +0100

    done tracking

--- tracker/models.py
+++ tracker/models.py
@@ -16,13 +16,20 @@ class Commodity(models.Model):
     
     def save(self, *args, **kwargs):
         one_hour_ago = timezone.now() -timezone.timedelta(hours=1)
-        one_year_ago = timezone.now() -timezone.timedelta(days=1)
+        one_day_ago = timezone.now() -timezone.timedelta(days=1)
         seven_days_ago = timezone.now() -timezone.timedelta(days=7)
 
 
-        previous_commodity_price = Commodity.objects.filter(name=self.name, time__lte=one_hour_ago).order_by('-time').first()
+        one_hour_previous_commodity_price = Commodity.objects.filter(name=self.name, time__lte=one_hour_ago).order_by('-time').first()
 
-        if previous_commodity_price:
-            self.price_1_hour_ago =previous_commodity_price.current_price
+        if one_hour_previous_commodity_price:
+            self.price_1_hour_ago =one_hour_previous_commodity_price.current_price
+
+        one_day_previous_commodity_price = Commodity.objects.filter(name=self.name, time__lte=one_day_ago).order_by('-time').first()
+
+        if one_day_previous_commodity_price:
+            self.price_1_day_ago =one_day_previous_commodity_price.current_price
+
+        
         
         super().save(*args, **kwargs)
--- tracker/serializers.py
+++ tracker/serializers.py
@@ -4,37 +4,49 @@ from .models import Commodity
 from datetime import timedelta
 import decimal
 
+# In your Django app's serializers.py file
+
+from rest_framework import serializers
+from .models import Commodity
+
 class CommoditySerializer(serializers.ModelSerializer):
-    percentage_change = serializers.SerializerMethodField()
+    one_hour_percentage_change = serializers.SerializerMethodField()
+    one_day_percentage_change = serializers.SerializerMethodField()
+    one_week_percentage_change = serializers.SerializerMethodField()
+    
+
     class Meta:
         model = Commodity
-        fields = ['name', 'current_price', 'time', 'percentage_change']
+        fields = ['id', 'name', 'current_price',  'time', 'one_hour_percentage_change', 'one_week_percentage_change', 'one_day_percentage_change']
 
-    def get_percentage_change(self, instance):
+    def get_one_hour_percentage_change(self, instance):
         current_price = instance.current_price
-
-        previous_price = self.get_previous_price
-
-        if previous_price:
+        previous_price = instance.price_1_hour_ago  # Get the previous price
+        if previous_price is not None and current_price is not None and previous_price != 0:
             percentage_change = ((current_price - previous_price) / previous_price) * 100
             return percentage_change
-        else: 
-            return None
+        else:
+            return None  # If any price is None or previous price is zero, return None
+
+    def get_one_day_percentage_change(self, instance):
+        current_price = instance.current_price
+        previous_price = instance.price_1_day_ago  # Get the previous price
+        if previous_price is not None and current_price is not None and previous_price != 0:
+            percentage_change = ((current_price - previous_price) / previous_price) * 100
+            return percentage_change
+        else:
+            return None  # If any price is None or previous price is zero, return None
         
-    def get_previous_price(self, instance):
+    def get_one_week_percentage_change(self, instance):
+        current_price = instance.current_price
+        previous_price = instance.price_7_days_ago  # Get the previous price
+        if previous_price is not None and current_price is not None and previous_price != 0:
+            percentage_change = ((current_price - previous_price) / previous_price) * 100
+            return percentage_change
+        else:
+            return None  # If any price is None or previous price is zero, return None
 
-        if instance.time:
-           one_hour_ago = decimal(200)
-           one_day_ago = decimal(500)
-           #one_hour_ago = instance.time - timedelta(hours=1)
-           #one_day_ago = instance.time - timedelta(days=1)
 
-           if instance >one_hour_ago:
-               print(one_hour_ago)
-               return one_hour_ago
-           elif instance.time >one_day_ago:
-               return one_day_ago
-        return None
 
 
     def to_representation(self, instance):
